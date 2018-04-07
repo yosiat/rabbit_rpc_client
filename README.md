dd97e8bb91e3374a79dbc1e08940db0d64efdceb
Yosi Attias <yosy101@gmail.com>
2018-04-07 19:42:11 +0300

    Re-use rabbitmq connection & one reply consumer

--- README.md
+++ README.md
@@ -5,48 +5,50 @@ Playing with Rabbit RPC client
 
 ## ConcurrentClient
 
-Based on concurrent-ruby Map + IVar
+- concurrent: uses concurrent-ruby map + ivar
+- one rabbitmq connection
+- one rabbit reply consumer
 
 ```
 Warming up --------------------------------------
-          new-client    11.000  i/100ms
-     existing-client   149.000  i/100ms
+          new-client   158.000  i/100ms
+     existing-client   158.000  i/100ms
 Calculating -------------------------------------
-          new-client    126.560  (± 9.5%) i/s -    627.000  in   5.001786s
-     existing-client    236.695k (±15.0%) i/s -      1.134M in   4.993230s
+          new-client      1.544k (± 4.0%) i/s -      7.742k in   5.023626s
+     existing-client    245.078k (±16.1%) i/s -      1.170M in   4.994396s
 
 Comparison:
-     existing-client:   236695.1 i/s
-          new-client:      126.6 i/s - 1870.21x  slower
+     existing-client:   245077.7 i/s
+          new-client:     1543.6 i/s - 158.77x  slower
 ```
 
 ### Stress test
 
 *Client per thread:*
 ```
-Runtime:   0.303732   0.198723   0.502455 (  0.415927)
+Runtime:   0.034071   0.012984   0.047055 (  0.041922)
 
 Latency Stats
- 50.000%          357
- 75.000%          367
- 90.000%          380
- 99.000%          383
- 99.900%          384
- 99.990%          384
- 99.999%          384
-100.000%          384
+ 50.000%           19
+ 75.000%           20
+ 90.000%           20
+ 99.000%           21
+ 99.900%           21
+ 99.990%           21
+ 99.999%           21
+100.000%           21
 ```
 
 *Same client:*
 
 ```
-Runtime:   0.024754   0.010672   0.035426 (  0.031249)
+Runtime:   0.023474   0.009739   0.033213 (  0.029422)
 
 Latency Stats
- 50.000%           14
- 75.000%           15
- 90.000%           15
- 99.000%           16
+ 50.000%           15
+ 75.000%           16
+ 90.000%           16
+ 99.000%           17
  99.900%           17
  99.990%           17
  99.999%           17
--- concurrent_client.rb
+++ concurrent_client.rb
@@ -4,47 +4,91 @@ require 'bunny'
 require 'oj'
 require 'concurrent'
 require 'securerandom'
+require 'singleton'
 
-class ConcurrentClient
-  def initialize(service_name)
-    @conn = Bunny.new
-    @conn.start
-    ch = @conn.create_channel
+class RabbitMQConnection
+  include Singleton
 
-    @x = ch.default_exchange
-    @reply_queue = ch.queue('', exclusive: true)
-    @service_name = service_name
+  attr_reader :connection
+  attr_reader :default_channel, :exchange
 
-    futures = @futures = Concurrent::Map.new
+  def initialize
+    @connection = Bunny.new
+    @connection.start
 
-    @timeout = nil
+    @default_channel = @connection.create_channel
+    @exchange = @default_channel.default_exchange
+  end
 
+  def close
+    @connection.stop
+    @connection.close
+  end
+end
+
+class RpcReplyConsumer
+  include Singleton
+
+  def initialize
+    @reply_queue = RabbitMQConnection.instance.default_channel.queue('', exclusive: true)
+    @futures = Concurrent::Map.new
+
+    consume!
+  end
+
+  def name
+    @reply_queue.name
+  end
+
+  def create_waiter_for_id(id)
+    promise = Concurrent::IVar.new
+    @futures[id] = promise
+    promise
+  end
+
+  def close
+    @consumer.cancel
+  end
+
+  private
+
+  def consume!
     @consumer = @reply_queue.subscribe(manual_ack: false) do |_delivery_info, properties, payload|
       correlation_id = properties[:correlation_id]
-      promise = futures[correlation_id]
+      promise = @futures[correlation_id]
+      raise "Empty promise for #{correlation_id}" if promise.nil?
 
       promise.set payload
-      futures.delete correlation_id
+      @futures.delete correlation_id
     end
   end
+end
 
-  def call(message)
-    promise = Concurrent::IVar.new
+class ConcurrentClient
+  DEFAULT_TIMEOUT = 60_000
 
+  def initialize(service_name)
+    @service_name = service_name
+  end
+
+  def call(message, timeout = DEFAULT_TIMEOUT)
     call_id = SecureRandom.uuid
-    @futures.put call_id, promise
+    promise = RpcReplyConsumer.instance.create_waiter_for_id call_id
 
-    @x.publish(Oj.dump(message),
-               correlation_id: call_id,
-               content_type: 'application/json',
-               routing_key: @service_name,
-               reply_to: @reply_queue.name)
+    dispatch message, correlation_id: call_id, reply_to: RpcReplyConsumer.instance.name
 
-    Oj.load(promise.value(@timeout))
+    Oj.load(promise.value(timeout))
   end
 
-  def stop
-    @consumer.cancel
-    @conn.close
+  def dispatch(message, options = {})
+    RabbitMQConnection.instance.exchange.publish(
+      Oj.dump(message),
+      options.merge(
+        content_type: 'application/json',
+        routing_key: @service_name
+      )
+    )
   end
+
+  def stop; end
 end
